018e549c891e04685694e4df14e31012d357649b
Frederik Ernst <frederik.ernst27@gmail.com>
2026-05-04 00:15:37 +0200

    Add trip DTOs and entity

---
+++ backend/src/main/java/com/sep/sep_backend/dto/TripCreateDto.java
@@ -0,0 +1,43 @@
+package com.sep.sep_backend.dto;
+
+import java.time.LocalDate;
+
+public class TripCreateDto {
+
+    private String name;
+    private String destination;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
