91ec572c97d3011fd56766ee11bc865a3a3d2b13
Allan Burfeind Bimete <allan.burfeindbimete@stud.uni-due.de>
2026-05-05 16:51:01 +0200

    findByUserId und findByTripId Methoden im Kalendereinträge repository hinzugefügt

--- backend/src/main/java/com/sep/sep_backend/CalenderEntry/CalenderEntryRepository.java
+++ backend/src/main/java/com/sep/sep_backend/CalenderEntry/CalenderEntryRepository.java
@@ -2,5 +2,9 @@ package com.sep.sep_backend.CalenderEntry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CalenderEntryRepository extends JpaRepository<CalenderEntry,Long> {
+    List<CalenderEntry> findByUserId(Long userId);
+    List<CalenderEntry> findByTripId(Long tripId);
 }
