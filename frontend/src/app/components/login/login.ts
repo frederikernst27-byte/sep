1ccc28132fd8681743700aaa7b58e4b8602acc93
Caren El Moussaoui <caren.elmoussaoui@stud.uni-due.de>
2026-05-06 21:47:45 +0200

    Login-Seite Frontend erstellt

---
+++ frontend/src/app/components/login/login.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  isLoading: boolean = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onLogin(): void {
+    this.errorMessage = '';
+    this.isLoading = true;
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: (user) => {
+        this.isLoading = false;
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = 'Login fehlgeschlagen. Bitte E-Mail und Passwort prüfen.';
+      }
+    });
+  }
+}
