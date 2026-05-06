4031b5aab033dc49ea532ad4939bc4ecb55550a0
Allan Burfeind Bimete <allan.burfeindbimete@stud.uni-due.de>
2026-05-06 02:39:01 +0200

    services und model überarbeitet und sauber in die ordnerstruktur eingefügt + model mit nötigen feldern befüllt

---
+++ frontend/src/app/model/calendar-entry.model.spec.ts
@@ -0,0 +1,7 @@
+import { CalendarEntry } from './calendar-entrymodel';
+
+describe('CalendarEntry', () => {
+  it('should create an instance', () => {
+    expect(new CalendarEntry()).toBeTruthy();
+  });
+});
