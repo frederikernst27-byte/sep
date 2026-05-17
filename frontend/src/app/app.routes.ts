e62efa262906a20933e662b5b348d29f43f93c91
Caren El Moussaoui <caren.elmoussaoui@stud.uni-due.de>
2026-05-17 15:24:45 +0200

    URL-Login-Route hinzugefügt

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AddTripComponent } from './components/add-trip/add-trip';
 import { CalendarView } from './components/calendar-view/calendar-view';
+import {Login} from './components/login/login';
 
 export const routes: Routes = [
   { path: 'calendar', component: CalendarView },
+  { path: 'login', component: Login},
   { path: 'add-trip', component: AddTripComponent },
   { path: '', redirectTo: 'calendar', pathMatch: 'full' }  // optional
 ];
