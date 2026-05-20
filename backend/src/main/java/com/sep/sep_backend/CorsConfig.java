a8f49a03346955d5235175318078b14434ce893e
Lennart Rüsing <lennart.ruesing@stud.uni-due.de>
2026-05-20 15:02:43 +0200

    add-trip fix // we probably need a paid AI subscription for the extraction, but manually works perfectly fine

--- backend/src/main/java/com/sep/sep_backend/CorsConfig.java
+++ backend/src/main/java/com/sep/sep_backend/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -14,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
             }
         };
     }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { AddTripComponent } from './components/add-trip/add-trip';
 import { CalendarView } from './components/calendar-view/calendar-view';
 import { DashboardComponent } from './components/dashboard/dashboard';
-import {Login} from './components/login/login';
+import { Login } from './components/login/login';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'calendar', component: CalendarView },
-  { path: 'login', component: Login},
+  { path: 'login', component: Login },
   { path: 'add-trip', component: AddTripComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }  // optional
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
 ];
